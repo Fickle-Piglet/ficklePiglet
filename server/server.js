02be1be3b76603d9c38486e27c60300a284b8c9c
joksina <jksalaudeen@yahoo.com>
2016-01-24 10:05:16 -0800

    some changes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var port = 5050;
 
+app.use(express.static(path.join(__dirname, '/../client')));
 
 app.listen(port, function(){
     console.log('Listening on port '+ port);
