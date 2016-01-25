6712f8da8c3143b7eef07c223bce624247f4494f
joksina <jksalaudeen@yahoo.com>
2016-01-25 15:15:55 -0800

    adding a search input

---
+++ client/app/Search/search.js
@@ -0,0 +1,9 @@
+// angular.module('fickle.search', [])
+
+// .controller('searchController', function ($scope, tags) {
+//   // Your code here
+//   $scope.data = {};
+
+//   $scope.getTags = function () {
+//    }
+// });
---
+++ client/app/app.js
@@ -0,0 +1,8 @@
+// angular.module('fickle', ['fickle.search'])
+// .config(function ($stateProvider) {
+// 	$stateProvider
+// 		.when('/', {
+// 			templateUrl: 'app/Search/search.html',
+// 			controller: 'searchController'
+// 	})
+// })
