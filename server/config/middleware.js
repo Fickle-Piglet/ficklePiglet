c2ab574afce874c989b3e3cd73ea43934d1ed4ac
Joshua Huang <jphuangjr@gmail.com>
2016-01-25 20:09:25 -0800

    Added package.json changes

---
+++ server/config/middleware.js
@@ -0,0 +1,8 @@
+var bodyParser = require("body-parser")
+
+module.exports = function (app, express) {
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(express.static(__dirname + '/../../client'));
+};
