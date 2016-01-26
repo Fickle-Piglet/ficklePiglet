09cac54ab1a923269850b62fab63b274c8a793ff
Tessa Serdar <tessajeanserdar@gmail.com>
2016-01-25 20:06:40 -0800

    added files for login and server routes

---
+++ client/login/login.js
@@ -0,0 +1,37 @@
+angular.module('app.auth', ['app.services'])
+
+.controller('AuthController', function ($scope, $http, $location) {
+
+
+  // login function to be called when input form submitted
+  $scope.login = function (user) {
+    $scope.error = '';
+    if(!user) {
+      var userData = {
+        "username":$scope.username,
+        "password":$scope.password
+      }
+    } 
+    console.log("Attempting to login", userData)
+    Auth.login(userData)
+      .then(function(message){
+          $scope.clearFields();
+          $scope.error = message;
+      })
+  };
+
+  // sign up function to be called when input form submitted
+  $scope.signup = function () {
+    $scope.signUpError = '';
+    var userData = {
+      "username":$scope.signUpUsername,
+      "password":$scope.signUpPassword
+    }
+    console.log('User entered signup data',userData)
+    Auth.signup(userData)
+    .then(function(message){
+      $scope.clearFields();
+      $scope.signUpError = message;
+    })
+  };
+});
---
+++ client/services.js
@@ -0,0 +1,31 @@
+angular.module('app.services', [])
+
+.factory('Auth', function ($http, $location, $window) {
+
+    var signin = function (user) {
+      return $http({
+          method: 'POST',
+          url: '/api/users/signin',
+          data: user
+        })
+        .then(function (resp) {
+          return resp.data.token;
+        });
+    };
+
+    var signup = function (user) {
+      return $http({
+          method: 'POST',
+          url: '/api/users/signup',
+          data: user
+        })
+        .then(function (resp) {
+          return resp.data.token;
+        });
+    };
+
+    return {
+      signin: signin,
+      signup: signup,
+    };
+});
