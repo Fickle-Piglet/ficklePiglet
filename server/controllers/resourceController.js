0b395617f1ca19025d8b986f7108b7911c85274f
Joshua Huang <jphuangjr@gmail.com>
2016-01-26 11:41:28 -0800

    Updated file structure. Took database setup out of server and put it in its own file.

--- server/controllers/resourceController.js
+++ server/controllers/resourceController.js
@@ -1,5 +1,4 @@
-var neo4j = require('node-neo4j');
-var db = new neo4j("http://FicklePiglet:WCdiwRlhygf4M6g5Itvi@ficklepiglet.sb02.stations.graphenedb.com:24789");
+var db = require("../../server/db/db.js")
 
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -3,11 +3,9 @@ var path = require('path');
 var app = express();
 var port = 5050;
 var scraper = require("../scripts/scraper/scraper")
+var db = require("../server/db/db.js")
 
 app.use(express.static(path.join(__dirname, '/client')));
-
-var neo4j = require('node-neo4j');
-var db = new neo4j("http://FicklePiglet:WCdiwRlhygf4M6g5Itvi@ficklepiglet.sb02.stations.graphenedb.com:24789");
 
 app.listen(port, function(){
     console.log('Listening on port '+ port);
@@ -27,7 +25,7 @@ setTimeout(function(){
 //sample query to return everything
 db.cypherQuery("START n=node(*) RETURN n;", function(err, result){
     if(err) throw err;
-    //console.log(result.data); // delivers an array of query results
+    console.log(result.data); // delivers an array of query results
 });
 
 
