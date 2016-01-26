23e8cf42c72208f3b349f89e20a31a72887e597c
Tessa Serdar <tessajeanserdar@gmail.com>
2016-01-26 14:42:23 -0800

    fixing commit

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,12 +1,28 @@
+var db = require("../db/db.js")
+
 module.exports = {
     signin : function(req, res){
         //TODO: Write signin query
 
     },
     signup: function(req, res){
-        //TODO: Write signup query
-        console.log("Got request", req.body)
-        res.send("what up")
+      var userInfo = req.body
+      db.cypherQuery("MATCH (n:User) WHERE n.firstname={firstname} and n.lastname={lastname} and n.username={username} and n.email={email} and n.password={password} return n", userInfo , function(err, result){
+            if(err) {
+              res.status(404).json(err);
+            } else if (result.data.length!==0) {
+              console.log(result)
+              res.status(400).send("Already a user. Please sign in.");
+            } else {
+              db.cypherQuery("create (n:User {firstname:{firstname},lastname:{lastname},username:{username},email:{email},password:{password}}) return n", userInfo , function(err, result){
+                    if(err) {
+                      res.status(404).json(err);
+                    } else {
+                      res.status(200);
+                    }
+                });           
+            }
+        });
     },
     logout: function(req, res){
         //TODO: Write logout function
