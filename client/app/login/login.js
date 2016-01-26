c8b77ece4b6f3434800a281d41f6cc1fbb1b8197
Tessa Serdar <tessajeanserdar@gmail.com>
2016-01-26 11:19:55 -0800

    adding login front-end

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -4,34 +4,34 @@ angular.module('fickle.auth', [])
 
 
   // login function to be called when input form submitted
-  // $scope.login = function (user) {
-  //   $scope.error = '';
-  //   if(!user) {
-  //     var userData = {
-  //       "username":$scope.username,
-  //       "password":$scope.password
-  //     }
-  //   } 
-  //   console.log("Attempting to login", userData)
-  //   Auth.login(userData)
-  //     .then(function(message){
-  //         $scope.clearFields();
-  //         $scope.error = message;
-  //     })
-  // };
+  $scope.login = function (user) {
+    $scope.error = '';
+    if(!user) {
+      var userData = {
+        "username":$scope.username,
+        "password":$scope.password
+      }
+    } 
+    console.log("Attempting to login", userData)
+    Auth.login(userData)
+      .then(function(message){
+          $scope.clearFields();
+          $scope.error = message;
+      })
+  };
 
   // sign up function to be called when input form submitted
-  // $scope.signup = function () {
-  //   $scope.signUpError = '';
-  //   var userData = {
-  //     "username":$scope.signUpUsername,
-  //     "password":$scope.signUpPassword
-  //   }
-  //   console.log('User entered signup data',userData)
-  //   Auth.signup(userData)
-  //   .then(function(message){
-  //     $scope.clearFields();
-  //     $scope.signUpError = message;
-  //   })
-  // };
+  $scope.signup = function () {
+    $scope.signUpError = '';
+    var userData = {
+      "username":$scope.signUpUsername,
+      "password":$scope.signUpPassword
+    }
+    console.log('User entered signup data',userData)
+    Auth.signup(userData)
+    .then(function(message){
+      $scope.clearFields();
+      $scope.signUpError = message;
+    })
+  };
 });
