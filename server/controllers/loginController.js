8ce7282fe9a3e87b3381df4f1a9d262eb7f6a2bc
Tessa Serdar <tessajeanserdar@gmail.com>
2016-01-26 12:09:24 -0800

    set up basic signup route

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,9 +1,12 @@
 module.exports = {
     signin : function(req, res){
         //TODO: Write signin query
+
     },
     signup: function(req, res){
         //TODO: Write signup query
+        console.log("Got request", req.body)
+        res.send("what up")
     },
     logout: function(req, res){
         //TODO: Write logout function
