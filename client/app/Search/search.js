c02288d5f1243132139fe2e1c952402349f1e480
joksina <jksalaudeen@yahoo.com>
2016-01-26 16:41:17 -0800

    fickle sign in

--- client/app/Search/search.js
+++ client/app/Search/search.js
@@ -1,3 +1,16 @@
-angular.module('fickle', ['ngMaterial'])
-	.controller('searchController', function($scope) {
+angular.module('fickle.search',['ngMaterial', 'ngMessages'])
+
+.controller('searchController', function($scope) {
+  $scope.items = ['Science', 'Technology', 'Engineering', 'Math'];
+    $scope.selected = [];
+
+    $scope.toggle = function (item, list) {
+      var idx = list.indexOf(item);
+      if (idx > -1) list.splice(idx, 1);
+      else list.push(item);
+    };
+
+    $scope.exists = function (item, list) {
+      return list.indexOf(item) > -1;
+    };
 });
