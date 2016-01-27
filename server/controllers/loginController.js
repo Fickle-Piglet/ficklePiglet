7f0ea6a35695fff5bcb3ef9e82261f470ca01321
Tessa Serdar <tessajeanserdar@gmail.com>
2016-01-27 15:02:50 -0800

    fixing bug - login

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -15,17 +15,18 @@ module.exports = {
     },
     signup: function(req, res){
       var userInfo = req.body
-      db.cypherQuery("MATCH (n:User) WHERE n.firstname={firstname} and n.lastname={lastname} and n.username={username} and n.email={email} and n.password={password} return n", userInfo , function(err, result){
+      db.cypherQuery("MATCH (n:User) WHERE n.firstname={firstname} and n.lastname={lastname} and n.username={name} and n.email={email} and n.password={pword} return n", userInfo , function(err, result){
             if(err) {
-              res.status(404).json(err);
+              res.sendStatus(404).json(err);
             } else if (result.data.length!==0) {
               res.send("existing");
             } else {
-              db.cypherQuery("create (n:User {firstname:{firstname},lastname:{lastname},username:{username},email:{email},password:{password}}) return n", userInfo , function(err, result){
+              db.cypherQuery("create (n:User {firstname:{firstname},lastname:{lastname},username:{name},email:{email},password:{pword}}) return n", userInfo , function(err, result){
                     if(err) {
-                      res.status(404).json(err);
+                      res.sendStatus(404).json(err);
+                      console.log(err);
                     } else {
-                      res.status(200);
+                      res.sendStatus(200);
                     }
                 });           
             }
