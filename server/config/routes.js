c1897ae528891ab8acf4c771cdc6b5f3aae208c3
Joey Leung <joeyleung8@gmail.com>
2016-01-27 16:08:44 -0800

    Added route comments

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,6 +7,13 @@ module.exports = function(app, express){
     app.post("/insert", resourceController.insertResource);
 
     //GetResource
+    //EX:
+    //   [ { name: '60-Second Space',
+    //   url: 'https://itunes.apple.com/us/podcast/60-second-space/id434439476?mt=2&uo=4',
+    //   _id: 4091 },
+    //   { name: 'Chalk Talk Physics Podcast',
+    //   url: 'https://itunes.apple.com/us/podcast/chalk-talk-physics-podcast/id262782016?mt=2&uo=4',
+    //   _id: 4054 }]
     app.post('/getResource', resourceController.getResource);
 
     //Tags
