066509cf784a48a4c4028c4f82aec01fe1d19df0
joksina <jksalaudeen@yahoo.com>
2016-01-29 19:37:10 -0800

    fixing conflicts on res.js

--- client/app/user/user.js
+++ client/app/user/user.js
@@ -6,4 +6,17 @@ angular.module('fickle.user',['ngMaterial', 'ngMessages'])
   .then(function (user) {
     console.log(user);
   });
+
+  $scope.like = function(resource){
+    var user = JSON.parse(window.localStorage.getItem('com.fickle'));
+    User.getLikes()
+    .then(function(message){
+      if(message ===200){
+      	console.log("yayy")
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
 });
