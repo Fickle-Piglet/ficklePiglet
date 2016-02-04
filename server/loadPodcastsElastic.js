94c48b9036d507cdffaa666609a7da26595c06ee
Joey Leung <joeyleung8@gmail.com>
2016-02-04 10:56:59 -0800

    Added Tags & Shows to Database

--- server/loadPodcastsElastic.js
+++ server/loadPodcastsElastic.js
@@ -1,16 +1,16 @@
 var db = require("../server/db/db.js");
 var elastic = require("../server/controllers/esearch");
 
-db.cypherQuery("MATCH (n:Resource) RETURN n", function (err, result) {
+db.cypherQuery("Match (t:Tag) return t", function (err, result) {
  if (err) {
-   throw error;
+   throw err;
  } else  {
-  bulkLoadPodcasts(result.data)
+  bulkLoadPodcasts(result.data);
  }
 });
         
 function bulkLoadPodcasts(podcasts) {
-  console.log("podcasts in main",podcasts)
+  console.log("podcasts in main",podcasts);
 
   elastic.indexExists().then(function (exists) {  
     if (exists) {
@@ -21,16 +21,44 @@ function bulkLoadPodcasts(podcasts) {
       //Add a few titles for the autocomplete
       //elasticsearch offers a bulk functionality as well, but this is for a different time
       podcasts.map(function (podcast) {
-        console.log("Podcast:",podcast)
-        return elastic.addDocument({
-          name: podcast.name,
-          url: podcast.url,
-          feedUrl: podcast.feedUrl,
-          thumbnail : podcast.thumbnail,
-          metadata: {
-            titleLength: Object.keys(podcast).length
-          }
-        });
+        if (podcast.url){
+          return elastic.addDocument({
+            name: podcast.name,
+            url: podcast.url,
+            feedUrl: podcast.feedUrl,
+            thumbnail : podcast.thumbnail,
+            metadata: {
+              titleLength: Object.keys(podcast).length
+            }
+          });
+        } else {
+          return elastic.addDocument({
+            name: podcast.name,
+            url: null,
+            feedUrl: null,
+            thumbnail : null,
+            metadata: {
+              titleLength: Object.keys(podcast).length
+            }
+          });
+        }
+      });
+      db.cypherQuery("Match (r:Resource) RETURN r", function (err, result) {
+       if (err) {
+         throw err;
+       } else  {
+         result.data.map(function (podcast) {
+          return elastic.addDocument({
+            name: podcast.name,
+            url: podcast.url,
+            feedUrl: podcast.feedUrl,
+            thumbnail : podcast.thumbnail,
+            metadata: {
+              titleLength: Object.keys(podcast).length
+            }
+          });
+        }); 
+       }
       });
       return Promise.all(podcasts);
     });
