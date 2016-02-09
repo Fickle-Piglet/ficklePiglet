6e316b6efdb4723dbfea4103feffc72ca557dca0
joksina <jksalaudeen@yahoo.com>
2016-02-08 17:44:34 -0800

    spacing

--- client/app/Search/search.js
+++ client/app/Search/search.js
@@ -2,26 +2,25 @@ angular.module('fickle.search',['ngMaterial', 'ngMessages'])
 
 .controller('searchController', function($scope,Podcasts) {
   $scope.items = ['Science', 'Technology', 'Engineering', 'Math'];
-    $scope.selected = [];
-    $scope.tags = [];
-    $scope.results = [];
-    
-    Podcasts.getTags().then(function (data) {
-      $scope.tags = data;
-    });
-
-    $scope.sendTags = function() {
-      Podcasts.setTags($scope.selected);
-    }
-
-    $scope.toggle = function (item, list) {
-      var idx = list.indexOf(item);
-      if (idx > -1) list.splice(idx, 1);
-      else list.push(item.name);
-    };
-
-    $scope.exists = function (item, list) {
-      return list.indexOf(item) > -1;
-    };
+  $scope.selected = [];
+  $scope.tags = [];
+  $scope.results = [];
   
+  Podcasts.getTags().then(function (data) {
+    $scope.tags = data;
+  });
+
+  $scope.sendTags = function() {
+    Podcasts.setTags($scope.selected);
+  }
+
+  $scope.toggle = function (item, list) {
+    var idx = list.indexOf(item);
+    if (idx > -1) list.splice(idx, 1);
+    else list.push(item.name);
+  };
+
+  $scope.exists = function (item, list) {
+    return list.indexOf(item) > -1;
+  };  
 });
