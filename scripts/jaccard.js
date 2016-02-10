817edea6812f50f6daf0187f24a2ea1dddb38d85
Joey Leung <joeyleung8@gmail.com>
2016-02-09 17:25:13 -0800

    WORKING VERSION

--- scripts/jaccard.js
+++ scripts/jaccard.js
@@ -6,66 +6,81 @@ var users;
 
 
 var compare = function(userOne, userTwo,callback) {
-// console.log("1: ", userOne, "2: ", userTwo);
 var userOneLikes;
 var userTwoLikes;
 var UserOneDislikes;
 var UserTwoDislikes;
+var argumentsArr = [];
 // console.log("UserOne: ", userOne);
 // console.log("UserTwo: ", userTwo);
-  
-  db.cypherQuery("MATCH (u:User {username:'" + userOne + "'})-[r:HAS_DISLIKED]-(e:Episode) return e.title", function(err, res){
-    if (err) {console.log("Error: with ",userOne," ", err);}
-    userOneDislikes = res.data;
-    console.log("typeof userOneDislikes: ", typeof userOneDislikes);
-    
-    db.cypherQuery("MATCH (u:User {username:'" + userTwo + "'})-[r:HAS_DISLIKED]-(e:Episode) return e.title", function(err, res){
-      if (err) {console.log("Error: with ",userTwo," ", err);}
-      userTwoDislikes = res.data;
-      console.log("typeof userTwoDislikes: ", typeof userTwoDislikes);
+db.cypherQuery("MATCH (u:User) return u.username", function(err, res){
+  users = res.data; 
+  for (var i = 0; i < users.length - 1; i++) {
+    for (var j = i+1; j < users.length; j ++) {
+      argumentsArr.push([users[i], users[j]]);
+      console.log(users[i], users[j]);
+    }
+  }
+  console.log("All the stuff: ", argumentsArr.length);
+  inner(0);
+});
+  var inner = function(userPair) {
+    // console.log("jumped into inner compare function", users[userOne], users[userTwo]);
+    db.cypherQuery("MATCH (u:User {username:'" + argumentsArr[userPair][0] + "'})-[r:HAS_DISLIKED]-(e:Episode) return e.title", function(err, res){
+      if (err) {console.log("Error: with ",argumentsArr[userPair][0]," ", err);}
+      userOneDislikes = res.data;
+      // console.log("typeof userOneDislikes: ", typeof userOneDislikes);
+      
+      db.cypherQuery("MATCH (u:User {username:'" + argumentsArr[userPair][1] + "'})-[r:HAS_DISLIKED]-(e:Episode) return e.title", function(err, res){
+        if (err) {console.log("Error: with ",argumentsArr[userPair][1]," ", err);}
+        userTwoDislikes = res.data;
+        // console.log("typeof userTwoDislikes: ", typeof userTwoDislikes);
 
-      db.cypherQuery("MATCH (u:User {username:'" + userOne + "'})-[r:HAS_LIKED]-(e:Episode) return e.title", function(err, res){
-        if (err) {console.log("Error: with ",userOne," ", err);}
-        userOneLikes = res.data;
-        console.log("typeof userOnelikes: ", typeof userOneLikes);
+        db.cypherQuery("MATCH (u:User {username:'" + argumentsArr[userPair][0] + "'})-[r:HAS_LIKED]-(e:Episode) return e.title", function(err, res){
+          if (err) {console.log("Error: with ",argumentsArr[userPair][0]," ", err);}
+          userOneLikes = res.data;
+          // console.log("typeof userOnelikes: ", typeof userOneLikes);
 
-        db.cypherQuery("MATCH (u:User {username:'" + userTwo + "'})-[r:HAS_LIKED]-(e:Episode) return e.title", function(err, res){
-          if (err) {console.log("Error: with ",userTwo," ", err);}
-          userTwoLikes = res.data;
-          console.log("typeof userTwolikes: ", typeof userTwoLikes);
-          // console.log("userOneLikes: ", userOneLikes.length);
-          // console.log("userTwoLikes: ", userTwoLikes.length);
-          var union = _.union(userOneLikes, userTwoLikes, userOneDislikes, userTwoDislikes).length;
-          var like = _.intersection(userOneLikes, userTwoLikes).length;
-          var dislike = _.intersection(userOneDislikes, userTwoDislikes).length;
-          var likedislike = _.intersection(userOneLikes, userTwoDislikes).length;
-          var dislikelike = _.intersection(userOneDislikes, userTwoLikes).length;
-          var similarityIndex = (like + dislike - likedislike - dislikelike)/union; 
-          // console.log("DA ANSWER: ", similarityIndex);
+          db.cypherQuery("MATCH (u:User {username:'" + argumentsArr[userPair][1] + "'})-[r:HAS_LIKED]-(e:Episode) return e.title", function(err, res){
+            if (err) {console.log("Error: with ",argumentsArr[userPair][1]," ", err);}
+            userTwoLikes = res.data;
+            // console.log("typeof userTwolikes: ", typeof userTwoLikes);
+            // console.log("userOneLikes: ", userOneLikes.length);
+            // console.log("userTwoLikes: ", userTwoLikes.length);
+            var union = _.union(userOneLikes, userTwoLikes, userOneDislikes, userTwoDislikes).length;
+            var like = _.intersection(userOneLikes, userTwoLikes).length;
+            var dislike = _.intersection(userOneDislikes, userTwoDislikes).length;
+            var likedislike = _.intersection(userOneLikes, userTwoDislikes).length;
+            var dislikelike = _.intersection(userOneDislikes, userTwoLikes).length;
+            var similarityIndex = (like + dislike - likedislike - dislikelike)/union; 
+            // console.log("DA ANSWER: ", similarityIndex);
 
 
-          db.cypherQuery("MATCH (u1:User {username:'" + userOne + "'}), (u2:User {username:'" + userTwo + "'}) MERGE (u1)-[:SIMILARITY {score: '" + similarityIndex + "'}]->(u2)", function(err, res){
-            if (err) { console.log (err); }
-            console.log("got to the end");
-            callback(userOne, userTwo);
+            db.cypherQuery("MATCH (u1:User {username:'" + argumentsArr[userPair][0] + "'}), (u2:User {username:'" + argumentsArr[userPair][1] + "'}) MERGE (u1)-[:SIMILARITY {score: '" + similarityIndex + "'}]->(u2)", function(err, res){
+              if (err) { console.log (err); }
+              console.log("Connected: ", argumentsArr[userPair][0]," and ", argumentsArr[userPair][1]);
+              if (userPair + 1 < argumentsArr.length) {
+                inner(userPair + 1);
+              } else {
+                console.log("WE ALL DONE WOOOSAOJASDJODSAJOADS");
+              }
+            });
           });
         });
       });
     });
-  });
+  };
+
 };
+
+compare();
 
 
 function makeUserSimalarities () {
+  
   console.log("Starting Function");
-  var j;
-  var iterate = function(userOne,userTwo){
-    // console.log(userOne, " and ", userTwo, " are connected!");
-    console.log(j);
-    j ++;
-  };
   db.cypherQuery("MATCH (u:User) return u.username", function(err, res){
-    users = res.data;
+    users = res.data; 
     console.log("Got Users Back");
     // console.log("USERS: ", users[0]);
     // console.log("USERS TYPE: ",typeof users[0]);
@@ -78,12 +93,13 @@ function makeUserSimalarities () {
       while(j < users.length){
         // console.log("Calling compare on 1: ", users[i], "2: ", users[j]);
         compare(users[i], users[j], iterate);
+        j++;
       }  
       // }
     }
   });  
 }
-makeUserSimalarities();
+// makeUserSimalarities();
 
 
 
